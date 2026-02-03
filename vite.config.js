fde625c8eadbc77a2fc5f4186995474917f38217
Vlad <102878458+vladklopotenko@users.noreply.github.com>
2026-02-03 20:24:07 +0200

    UPD

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // GitHub Pages deploy for repo: kursova_klopotenko-main
-  base: '/kursova_klopotenko-main/',
+  base: '/kursova_klopotenko/',
 });
